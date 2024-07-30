e3c850737f08755bef43c3c9f0f4449105473849
skril <skril@macbook-air.tail6bb62.ts.net>
2024-07-30 17:10:16 +0300

    set up sbt project

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "graph-transformer"
+version := "0.1.0"
+scalaVersion := "3.4.2"
---
+++ src/main/scala/graphTransformer/Main.scala
@@ -0,0 +1,5 @@
+package graphTransformer
+
+object Main extends App {
+  println("Hello, Scala SBT project!")
+}
